c5fc01530f31752293c90a1f96737c216380e083
Alain Ravet <alain.ravet+github@gmail.com>
2012-03-12 21:33:24 +0100

    #list returns the (id + details) for each scheduled cron.io jobs

--- spec/list_crons_spec.rb
+++ spec/list_crons_spec.rb
@@ -4,6 +4,9 @@ describe Cron::Io::Cron do
   let(:existing_user_name) {'croniogem'}
   let(:existing_user_pwd ) {'secret'}
 
+  let(:existing_user_with_2_crons_name) {'croniogem2'}
+  let(:existing_user_with_2_crons_pwd ) {'secret'}
+
   describe "#list" do
 
     describe "for a virgin user" do
@@ -11,6 +14,18 @@ describe Cron::Io::Cron do
         result  = Cron::Io::Cron.list(existing_user_name, existing_user_pwd)
         result['code'].should == "200"
         result['parsed_response'].should == []
+      end
+    end
+
+
+    describe "for a user with 2 crons scheduled" do
+      it 'returns the 2 crons details' do
+        expected_crons_as_json =  [{"id"=>"4f5e5b2a04c11ff30e00006a", "name"=>"my cron",   "url"=>"http://example.com", "schedule"=>"30-40 * * * *"},
+                                   {"id"=>"4f5e5b3c04c11ff30e00006d", "name"=>"my cron 2", "url"=>"http://example.com", "schedule"=>"20-40 * * * *"}
+                                  ]
+        result  = Cron::Io::Cron.list(existing_user_with_2_crons_name, existing_user_with_2_crons_pwd)
+        result['code'].should == "200"
+        result['parsed_response'].should == expected_crons_as_json
       end
     end
 
