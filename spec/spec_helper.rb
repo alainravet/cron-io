91ad61baa0354e13abf327a3bec604d374525e23
Alain Ravet <alain.ravet+github@gmail.com>
2012-03-13 10:40:51 +0100

    configure vcr (spec/vcr_cassette_library << cassettes)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,23 @@ Spec::Runner.configure do |config|
 end
 
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each {|f| require f}
+
+require 'vcr'
+require 'fakeweb'
+
+SPECS_DIR = File.expand_path(File.dirname(__FILE__))
+
+VCR_CASSETTES_DIR = File.join(SPECS_DIR, 'vcr_cassette_library')
+
+VCR.config do |c|
+  c.cassette_library_dir     = VCR_CASSETTES_DIR
+  c.stub_with                :fakeweb
+  c.ignore_localhost         = true
+  c.default_cassette_options = { :record => :none }
+  c.allow_http_connections_when_no_cassette = true
+end
+
+
+Spec::Runner.configure do |c|
+  c.extend VCR::RSpec::Macros
+end
